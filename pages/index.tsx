1e01064762ebb9e121ae4f7ea9165a01ec5bb434
kazustudy0107 <the.stone.monky.6317@gmail.com>
2023-01-21 15:16:32 +0900

    🎨 ざっくりとヘッダー部分を作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,36 @@
+import { Container, Group, Text } from "@mantine/core";
 import { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  return <div className="text-red-500">test</div>;
+  return (
+    <>
+      <Container className="xl">
+        <div className="flex h-20 items-center justify-center gap-60">
+          <Link
+            href="/"
+            className="font-mono text-2xl font-bold text-black no-underline"
+          >
+            My Web Site
+          </Link>
+          <Group>
+            <Link href="/profile" className="no-underline">
+              プロフィール
+            </Link>
+            <Link href="/blog" className="no-underline">
+              ブログ
+            </Link>
+            <Link href="/news" className="no-underline">
+              ポートフォリオ
+            </Link>
+            <Link href="/blog" className="no-underline">
+              お問い合わせ
+            </Link>
+          </Group>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default Home;
