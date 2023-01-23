0447bbaf0b5366e0859140dfae1d3725931d6f40
kazustudy0107 <the.stone.monky.6317@gmail.com>
2023-01-23 10:43:54 +0900

    🚧 ヘッダーをコンポーネント化

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/component/Header";
 import { usePosts } from "@/hooks/usePosts";
 import { Center, Container, Divider, Group, Image, Text } from "@mantine/core";
 import { NextPage } from "next";
@@ -30,44 +31,7 @@ const Home: NextPage = () => {
 
   return (
     <div>
-      {/* ヘッダー */}
-      <Container>
-        <div className="flex h-20 items-center justify-between">
-          <Link
-            href="/"
-            className="ml-3 font-mono text-2xl font-bold text-black no-underline"
-          >
-            My Web Site
-          </Link>
-          <Group>
-            <Link
-              href="/profile"
-              className=" text-black no-underline hover:text-blue-500"
-            >
-              プロフィール
-            </Link>
-            <Link
-              href="/blog"
-              className="text-black no-underline  hover:text-blue-500"
-            >
-              ブログ
-            </Link>
-            <Link
-              href="/news"
-              className="text-black no-underline  hover:text-blue-500"
-            >
-              ポートフォリオ
-            </Link>
-            <Link
-              href="/blog"
-              className="text-black no-underline  hover:text-blue-500"
-            >
-              お問い合わせ
-            </Link>
-          </Group>
-        </div>
-      </Container>
-
+        <Header />
       {/* ヒーロエリア */}
       {/* <Container className="xl"> */}
       <Image
