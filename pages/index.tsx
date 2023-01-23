a86573862a2ddedc3ca6c629babf51b8414f48ba
kazustudy0107 <the.stone.monky.6317@gmail.com>
2023-01-23 11:08:48 +0900

    🔥 不要なコードを削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,34 +3,32 @@ import { Header } from "@/component/Header";
 import { Hero } from "@/component/Hero";
 import { Info } from "@/component/Info";
 import { usePosts } from "@/hooks/usePosts";
-import { Center, Container, Divider, Group, Text } from "@mantine/core";
 import { NextPage } from "next";
-import Link from "next/link";
-import { BrandGithub, BrandInstagram, BrandTwitter } from "tabler-icons-react";
 
 const Home: NextPage = () => {
-  const Posts = () => {
-    const { data, error, isLoading, isEmpty } = usePosts();
+  // ダミーのブログ部分は後回し
+  // const Posts = () => {
+  //   const { data, error, isLoading, isEmpty } = usePosts();
 
-    console.log(data);
+  //   console.log(data);
 
-    if (isLoading) {
-      return <div>loading...</div>;
-    }
-    if (error) {
-      return <div>failed to load</div>;
-    }
-    if (isEmpty) {
-      return <div>no data</div>;
-    }
-    return (
-      <ol>
-        {data.map((post: any) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ol>
-    );
-  };
+  //   if (isLoading) {
+  //     return <div>loading...</div>;
+  //   }
+  //   if (error) {
+  //     return <div>failed to load</div>;
+  //   }
+  //   if (isEmpty) {
+  //     return <div>no data</div>;
+  //   }
+  //   return (
+  //     <ol>
+  //       {data.map((post: any) => (
+  //         <li key={post.id}>{post.title}</li>
+  //       ))}
+  //     </ol>
+  //   );
+  // };
 
   return (
     <div>
