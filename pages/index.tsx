0947d0b16e70d827e5c9c382bcd0883da0c191d2
kazustudy0107 <the.stone.monky.6317@gmail.com>
2023-01-23 11:03:18 +0900

    🚧 Footerをコンポーネント化

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/component/Footer";
 import { Header } from "@/component/Header";
 import { Hero } from "@/component/Hero";
 import { Info } from "@/component/Info";
@@ -33,59 +34,10 @@ const Home: NextPage = () => {
 
   return (
     <div>
-        <Header />
-        <Hero />
-        <Info/>
-
-
-      {/* フッター */}
-      <Divider my="lg" className="mt-20" />
-      <Container className="flex justify-between">
-        <Group className="ml-3">
-          <Link target="_blank" href="https://twitter.com/eigablog_kazu">
-            <BrandTwitter />
-          </Link>
-          <Link
-            target="_blank"
-            href="https://www.instagram.com/eigasukisuki_kazu/"
-          >
-            <BrandInstagram />
-          </Link>
-          <Link target="_blank" href="https://github.com/kazushige-s">
-            <BrandGithub />
-          </Link>
-        </Group>
-
-        <Group>
-          <Link
-            href="/profile"
-            className=" text-black no-underline hover:text-blue-500"
-          >
-            プロフィール
-          </Link>
-          <Link
-            href="/blog"
-            className="text-black no-underline  hover:text-blue-500"
-          >
-            ブログ
-          </Link>
-          <Link
-            href="/news"
-            className="text-black no-underline  hover:text-blue-500"
-          >
-            ポートフォリオ
-          </Link>
-          <Link
-            href="/blog"
-            className="text-black no-underline  hover:text-blue-500"
-          >
-            お問い合わせ
-          </Link>
-        </Group>
-      </Container>
-      <small>
-        <Text className="my-8 text-center">&copy;2023 My Web Site</Text>
-      </small>
+      <Header />
+      <Hero />
+      <Info />
+      <Footer />
     </div>
   );
 };
