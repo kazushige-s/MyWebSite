2ba95df35fa9264fec6635d9ce9920ff5772141d
kazustudy0107 <the.stone.monky.6317@gmail.com>
2023-01-26 14:29:22 +0900

    🎨 デザインを修正

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,20 +8,29 @@ export const Hero = () => {
       <Carousel slideSize="70%" slideGap="md" loop>
         <Carousel.Slide>
           <Image
-            src="https://source.unsplash.com/random/1480x480/"
+            src="https://source.unsplash.com/featured/?fashion"
             alt="Unsplash image"
+            width={1480}
+            height={480}
+            fit="cover"
           />
         </Carousel.Slide>
         <Carousel.Slide>
           <Image
-            src="https://source.unsplash.com/random/1480x480/"
+            src="https://source.unsplash.com/featured/?coffe"
             alt="Unsplash image"
+            width={1480}
+            height={480}
+            fit="cover"
           />
         </Carousel.Slide>
         <Carousel.Slide>
           <Image
-            src="https://source.unsplash.com/random/1480x480/"
+            src="https://source.unsplash.com/featured/?dog"
             alt="Unsplash image"
+            width={1480}
+            height={480}
+            fit="cover"
           />
         </Carousel.Slide>
       </Carousel>
