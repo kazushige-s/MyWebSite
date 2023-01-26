dfbf6c1527ab3d79ba6c66a383da9d39b8c8bc64
kazustudy0107 <the.stone.monky.6317@gmail.com>
2023-01-26 17:50:56 +0900

    ✨ ブログを作成！

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,55 @@
+import { Layout } from "@/src/components/Layout";
+import React from "react";
+import { client } from "@/src/libs/client";
+import { GetStaticProps, NextPage } from "next";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import Link from "next/link";
+import { Divider } from "@mantine/core";
+
+export type Blog = {
+  id: string;
+  name: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+};
+
+type Props = MicroCMSListResponse<Blog>;
+
+const blog: NextPage<Props> = (props) => {
+  return (
+    <Layout title="Blog">
+      <div>
+        <h1 className="font-bold">Blog</h1>
+        <ul>
+          {props.contents.map((content) => (
+            <li key={content.id} className="mt-3">
+              <Link
+                href={`/blog/${content.id}`}
+                className="
+                text-lg
+                text-black
+                hover:text-blue-500"
+                no-underline
+              >
+                {content.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Layout>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data = await client.getList<Blog>({ endpoint: "blogs" });
+  return {
+    props: data,
+  };
+};
+
+export default blog;
