3d84aae6dcb724dd6332fc9f218a7e82ec21ebc8
kazustudy0107 <the.stone.monky.6317@gmail.com>
2023-01-26 10:20:47 +0900

    🎨 デザインを微修正

--- src/components/SNS/SnsItem.tsx
+++ src/components/SNS/SnsItem.tsx
@@ -1,4 +1,4 @@
-import { Group } from "@mantine/core";
+import { ActionIcon, Group } from "@mantine/core";
 import Link from "next/link";
 import React from "react";
 import { BrandGithub, BrandInstagram, BrandTwitter } from "tabler-icons-react";
@@ -8,16 +8,22 @@ export const SnsItem = () => {
     <div className="flex">
       <Group className="ml-3">
         <Link target="_blank" href="https://twitter.com/eigablog_kazu">
-          <BrandTwitter />
+          <ActionIcon color="dark" size="lg">
+            <BrandTwitter />
+          </ActionIcon>
         </Link>
         <Link
           target="_blank"
           href="https://www.instagram.com/eigasukisuki_kazu/"
         >
-          <BrandInstagram />
+          <ActionIcon color="dark" size="lg">
+            <BrandInstagram />
+          </ActionIcon>
         </Link>
         <Link target="_blank" href="https://github.com/kazushige-s">
-          <BrandGithub />
+          <ActionIcon color="dark" size="lg">
+            <BrandGithub />
+          </ActionIcon>
         </Link>
       </Group>
     </div>
