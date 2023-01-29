4480d5535946bad805c0b9116390fa43b2407227
kazustudy0107 <the.stone.monky.6317@gmail.com>
2023-01-29 16:18:48 +0900

    🎨 デザインを微修正

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,21 +1,25 @@
-import { Accordion, Center, Container, Group, Text } from "@mantine/core";
+import {
+  Center,
+  Container,
+  Text,
+} from "@mantine/core";
 import React from "react";
 
 export const Info = () => {
   return (
     <>
-      <Container className="mx-auto mt-10">
-        <Container className="border-solid">
-          <Center>
-            <Text className="my-5 text-2xl font-bold">お知らせ</Text>
-          </Center>
-          <ol className="m-0 mb-5">
-            <li>ダークモードボタンは未対応(勉強中…)</li>
-            <li>テスト</li>
-            <li>テスト</li>
-            <li>テスト</li>
-            <li>テスト</li>
-          </ol>
+      <Container className="mx-auto mt-10 ">
+        <Container size="sm" className="border-solid">
+            <Center>
+              <Text className="my-5 text-2xl font-bold underline">お知らせ</Text>
+            </Center>
+            <ul className="m-0 mb-5">
+              <li>ダークモードボタンは未対応(勉強中…)</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+              <li>テスト</li>
+            </ul>
         </Container>
       </Container>
     </>
