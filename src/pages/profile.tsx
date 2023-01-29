38d8ffd9eb0140b88eeb7628127dd4216b22eda8
kazustudy0107 <the.stone.monky.6317@gmail.com>
2023-01-29 15:57:14 +0900

    🎨 レスポンシブ対応

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,11 +1,11 @@
 import { CareerData } from "@/src/components/Career/CareerData";
 import { Layout } from "@/src/components/Layout";
-import { Center, Group, Image, Table, Text } from "@mantine/core";
+import { Center, Grid, Group, Image, Table, Text } from "@mantine/core";
 import React from "react";
 
 const profile = () => {
-  const Career = CareerData.map((item) => (
-    <tr key={item.time}>
+  const Career = CareerData.map((item, i) => (
+    <tr key={i}>
       <td>{item.time}</td>
       <td>{item.company}</td>
       <td>{item.summary}</td>
@@ -14,24 +14,32 @@ const profile = () => {
 
   return (
     <Layout title="Profile">
+      {/* プロフィールエリア */}
       <Center>
-        <h2 className="mb-10 font-bold">プロフィール</h2>
+        <h2 className="mb-5 font-bold">プロフィール</h2>
       </Center>
-      <div className="flex gap-10">
-        <Image
-          src="https://source.unsplash.com/featured/?kitten/"
-          alt="profile image"
-          radius={90}
-          height={180}
-          width={180}
-        />
-        <Group>
-          <Text sx={{ fontFamily: "Noto Sans CJK JP" }}>
-            自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介
-          </Text>
-          <Text sx={{ fontFamily: "Noto Sans CJK JP" }}>名前</Text>
-        </Group>
-      </div>
+      <Grid justify="center" align="center">
+        <Grid.Col span="content">
+          <Image
+            src="https://source.unsplash.com/featured/?cat/"
+            alt="profile image"
+            radius={90}
+            height={180}
+            width={180}
+            className="m-5"
+          />
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Group>
+            <Text sx={{ fontFamily: "Noto Sans CJK JP" }}>
+              自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介、自己紹介
+            </Text>
+            <Text sx={{ fontFamily: "Noto Sans CJK JP" }}>名前</Text>
+          </Group>
+        </Grid.Col>
+      </Grid>
+
+      {/* 経歴エリア */}
       <Center>
         <h3 className="mt-10 font-bold">経歴</h3>
       </Center>
